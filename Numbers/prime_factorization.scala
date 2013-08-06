d39532d882b7885b003204c59438b43caabc7cc2
Dglgmut <florez.renan@gmail.com>
2013-08-06 00:05:39 -0300

    printing properly the prime factors of a number

--- Numbers/prime_factorization.scala
+++ Numbers/prime_factorization.scala
@@ -1,7 +1,8 @@
 object PrimeNumbersManipulator {
-  def factorsFor(number: Int): List[Any] = {
-    val elements: IndexedSeq[Int] = (2 to number).filter( i => (2 to  number).forall{o => i == o} )
-    elements.toList.init
+  def primeNumbersUntil(number: Int): List[Int] = (2 to number).filter( i => (2 to number).forall{o => (i == o || i % o > 0)} ).toList
+  def factorsFor(number: Int): List[Int] = {
+    val elements: List[Int] = primeNumbersUntil(number)
+    elements.filter( i => number % i == 0)
   }
 }
-println(PrimeNumbersManipulator.factorsFor(16))
+println(PrimeNumbersManipulator.factorsFor(10))
