2599cdfe7213144b3221cd0e9df236e36d1d3c6f
Dglgmut <florez.renan@gmail.com>
2013-08-06 22:30:55 -0300

    bad functional code prototype

--- Numbers/prime_factorization.scala
+++ Numbers/prime_factorization.scala
@@ -1,6 +1,20 @@
-def factorsFor(number: Int): IndexedSeq[Int] = {
+def divide(i: IndexedSeq[Int],o: Int): Int = {
+  if(i.length > 0){
+    if(o % i(0) == 0){
+      println(i(0)) 
+      divide(i, (o / i(0)))
+      i(0)
+    }
+    else 
+      divide(i.tail,o)
+  }
+  else
+    0
+}
+def factorsFor(number: Int): Unit = {
   val allNumbersList: Range = (2 to number)
-  allNumbersList.filter(  i => number % i == 0).filter(i => allNumbersList.forall{o => (i == o || i % o > 0)})
+  val divisibleNumbers = allNumbersList.filter(  i => number % i == 0)
+  divide(divisibleNumbers,number)
 }
 println("Enter a number to see its prime factors")
-println(factorsFor(readLine().toInt))
+factorsFor(readLine().toInt)
