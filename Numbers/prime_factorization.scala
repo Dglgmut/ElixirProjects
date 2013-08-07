69990fd6b2b58db9165f6ff75a6c83fcabe18caa
Dglgmut <florez.renan@gmail.com>
2013-08-06 21:09:16 -0300

    refactoring for better performance (currently can handle approx 10 digits)

--- Numbers/prime_factorization.scala
+++ Numbers/prime_factorization.scala
@@ -1,5 +1,5 @@
 object PrimeNumbersManipulator {
-  def primeNumbersUntil(number: Int): List[Int] = (2 to number).filter( i => (2 to number).forall{o => (i == o || i % o > 0)} ).toList
+  def primeNumbersUntil(number: Int): List[Int] = (2 to number).filter( i => (2 to number).forall{o => (i == o || i % o > 0) && i % number == 0} ).toList
   def factorsFor(number: Int): List[Int] = {
     val elements: List[Int] = primeNumbersUntil(number)
     elements.filter( i => number % i == 0)
