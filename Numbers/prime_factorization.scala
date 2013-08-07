6ea5839ff4b7e5880a323c06efc7edbc21607272
Dglgmut <florez.renan@gmail.com>
2013-08-06 22:04:09 -0300

    omitting conversions

--- Numbers/prime_factorization.scala
+++ Numbers/prime_factorization.scala
@@ -1,9 +1,6 @@
-object PrimeNumbersManipulator {
-  def primeNumbersUntil(number: Int): List[Int] = (2 to number).filter( i => (2 to number).forall{o => (i == o || i % o > 0) && i % number == 0} ).toList
-  def factorsFor(number: Int): List[Int] = {
-    val elements: List[Int] = primeNumbersUntil(number)
-    elements.filter( i => number % i == 0)
-  }
+def factorsFor(number: Int): IndexedSeq[Int] = {
+  val allNumbersList: Range = (2 to number)
+  allNumbersList.filter(  i => number % i == 0).filter(i => allNumbersList.forall{o => (i == o || i % o > 0)})
 }
 println("Enter a number to see its prime factors")
-println(PrimeNumbersManipulator.factorsFor(readLine().toInt))
+println(factorsFor(readLine().toInt))
