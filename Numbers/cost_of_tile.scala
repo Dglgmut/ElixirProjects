f2a8c068f3a5f6f35995d4f4a37de30e62e12151
Dglgmut <florez.renan@gmail.com>
2013-08-07 22:26:36 -0300

    printing total price

--- Numbers/cost_of_tile.scala
+++ Numbers/cost_of_tile.scala
@@ -1,5 +1,5 @@
 try {
-  println()
+  println(args(0).toInt * args(1).toInt * args(2).toInt)
 } catch {
   case e: ArrayIndexOutOfBoundsException => println("Enter the inputs in  the following order: Price Height Width")
 }
