a4a207edce1ce93db0eda37c87e04860143dab68
Dglgmut <florez.renan@gmail.com>
2013-08-07 22:34:18 -0300

    attempt to DRY args

--- Numbers/cost_of_tile.scala
+++ Numbers/cost_of_tile.scala
@@ -1,5 +1,11 @@
+def multiplyArgs(argsRange: Range): Int = {
+  if(argsRange.length > 0)
+    args(argsRange(0)).toInt * multiplyArgs(argsRange.tail)
+  else
+    1
+}
 try {
-  println(args(0).toInt * args(1).toInt * args(2).toInt)
+  println(multiplyArgs(0 to 2))
 } catch {
   case e: ArrayIndexOutOfBoundsException => println("Enter the inputs in  the following order: Price Height Width")
 }
