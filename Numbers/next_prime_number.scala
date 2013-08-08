ef7e36f011ff3ecea94de1f1a978d0290658bf27
Dglgmut <florez.renan@gmail.com>
2013-08-07 21:51:40 -0300

    creating base for filter

--- Numbers/next_prime_number.scala
+++ Numbers/next_prime_number.scala
@@ -1,7 +1,9 @@
-def readKeyAndPrintNextPrimeNumber():Unit = {
-  println(console.readVirtualKey())
-  readKeyAndPrintNextPrimeNumber
+val maxValue = 999999999
+def readKeyAndPrintNextPrimeNumber(primeNumbers: Range):Range = {
+  console.readVirtualKey()
+  println(primeNumbers(0))
+  readKeyAndPrintNextPrimeNumber(primeNumbers.tail)
 }
 println("This program will print prime numbers for each key stroke,'Ctrl + c to exit'")
 val console = new tools.jline.console.ConsoleReader()
-readKeyAndPrintNextPrimeNumber
+readKeyAndPrintNextPrimeNumber(2 to maxValue).filter( i => true)
