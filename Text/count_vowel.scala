3158dcc727f05d00f5bbb8bf5a4ac1e48b7a670a
Dglgmut <florez.renan@gmail.com>
2013-08-09 20:58:18 -0300

    creating count letter function

--- Text/count_vowel.scala
+++ Text/count_vowel.scala
@@ -1,3 +1,6 @@
+def countLetterAtString(phrase: String, letter: Char): Map[Char, Int] = {
+  Map(letter -> phrase.toCharArray.filer{l => letter.exists(l == _)}.length)
+}
 println("Enter a string")
 val vowel: List[Char] = List('a', 'e', 'i', 'o', 'u')
 val input: String = readLine()
