bc4e02a2bcbf195bd4f385bbee7929edfeec0209
Dglgmut <florez.renan@gmail.com>
2013-08-09 21:20:57 -0300

    filtering vowels

--- Text/count_vowel.scala
+++ Text/count_vowel.scala
@@ -1,7 +1,7 @@
-def countLetterAtString(phrase: String, letter: Char): Map[Char, Int] = {
-  Map(letter -> phrase.toCharArray.filer{l => letter.exists(l == _)}.length)
-}
+def countLetterAtString(phrase: String, letter: Char): Map[Char, Int] =
+  Map(letter -> phrase.filter(letter == _).length)
+
 println("Enter a string")
 val vowel: List[Char] = List('a', 'e', 'i', 'o', 'u')
 val input: String = readLine()
-println(input.toCharArray.filter{l => vowel.exists(l == _)}.length)
+vowel.foreach(v => println(countLetterAtString(input, v)))
