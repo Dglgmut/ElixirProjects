9c2ad5d6b919d873b8a8cd33c2f6cebf654b0628
Dglgmut <florez.renan@gmail.com>
2013-08-11 17:49:47 -0300

    defining get month

--- Numbers/mortgage.scala
+++ Numbers/mortgage.scala
@@ -1,2 +1,4 @@
+// TODO: use an immutable library
 import java.util.Calendar
 import java.text.SimpleDateFormat
+def dateToMonth(date: Calendar): String = new SimpleDateFormat("MMMM").format(date.getTime)
