afc88edcbd9a08db3f38a7d64d6d532fe7584359
Dglgmut <florez.renan@gmail.com>
2013-08-11 18:05:28 -0300

    creating function date to output

--- Numbers/mortgage.scala
+++ Numbers/mortgage.scala
@@ -1,4 +1,5 @@
 // TODO: use an immutable library
 import java.util.Calendar
 import java.text.SimpleDateFormat
-def dateToMonth(date: Calendar): String = new SimpleDateFormat("MMMM").format(date.getTime)
+def dateToOutput(date: Calendar): String = new SimpleDateFormat("MMMM yyyy").format(date.getTime)
+
