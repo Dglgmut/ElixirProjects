2af4bd6d07f4d435c80ec06fbd1b471ec22b45ec
Dglgmut <florez.renan@gmail.com>
2013-08-11 18:46:50 -0300

    refactoring everything

--- Numbers/mortgage.scala
+++ Numbers/mortgage.scala
@@ -1,8 +1,15 @@
 // TODO: use an immutable library
 import java.util.Calendar
 import java.text.SimpleDateFormat
+import java.util.Date
 
-def dateToOutput(date: Date): String = new SimpleDateFormat("MMMM yyyy").format(date)
+var timeNow = Calendar.getInstance
+def sumDateToOutput: String = {
+  timeNow.add(Calendar.MONTH, mortgageTerm)
+  new SimpleDateFormat("MMMM yyyy").format(timeNow.getTime)
+}
+def monthlyPayment: String = 
+  (mortgageAmount * (interestRate / 100 + 1) / mortgageTerm).toString
 
 println("Enter the mortgage amount")
 val mortgageAmount: Integer = readLine().toInt
@@ -10,3 +17,5 @@ println("Enter the interest rate")
 val interestRate: Float = readLine().toFloat
 println("Enter the mortgage term(months)")
 val mortgageTerm: Integer = readLine().toInt 
+
+println("You will pay monthly " + monthlyPayment + " until " + sumDateToOutput )
