a32b2887a61747daad70b3a4836d2f58c4325969
Dglgmut <florez.renan@gmail.com>
2013-08-11 18:13:19 -0300

    defining IO

--- Numbers/mortgage.scala
+++ Numbers/mortgage.scala
@@ -1,5 +1,12 @@
 // TODO: use an immutable library
 import java.util.Calendar
 import java.text.SimpleDateFormat
+
 def dateToOutput(date: Calendar): String = new SimpleDateFormat("MMMM yyyy").format(date.getTime)
 
+println("Enter the mortgage amount")
+val mortgageAmount: Integer = readLine().toInt
+println("Enter the interest rate")
+val interestRate: Float = readLine().toFloat
+println("Enter the mortgage term(months)")
+val mortgageTerm: Integer = readLine().toInt 
