bb519ac1ec2e6d45f43e8d78522aac08ebb12f97
Dglgmut <florez.renan@gmail.com>
2013-08-12 21:32:46 -0300

    defining value

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -1,3 +1,4 @@
 class Coins(coinType: Symbol) {
   private val coinTypes = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
+  def value: Double = coinTypes(coinType)
 }
