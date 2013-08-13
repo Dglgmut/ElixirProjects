ae21fc62355ba8b863c7a2239a21d422d71e2eea
Dglgmut <florez.renan@gmail.com>
2013-08-12 21:53:40 -0300

    creating companion object

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -1,5 +1,9 @@
 class Coins(coinType: Symbol) {
-  private val coinTypes = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
-  def value: Double = coinTypes(coinType)
-  def toOneDollar: Int = (1.0 / this.value).toInt
+  def value: Double = coinTypesDictionary(coinType)
+  def toOneDollar: Double = 1.0 / this.value
 }
+object Coins{
+  private val coinTypesDictionary = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
+  def coinTypes: Iterable[Symbol] = coinTypesDictionary.keys
+}
+println(Coins.coinTypes)
