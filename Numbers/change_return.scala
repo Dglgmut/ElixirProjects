13257626a4c33c12449b4a5a7b45f69e8bab6c59
Dglgmut <florez.renan@gmail.com>
2013-08-12 21:33:04 -0300

    defining toOneDollar

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -1,4 +1,5 @@
 class Coins(coinType: Symbol) {
   private val coinTypes = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
   def value: Double = coinTypes(coinType)
+  def toOneDollar: Int = (1.0 / this.value).toInt
 }
