614ec7b55b56825d2e4e90e5e3c12258342ced0f
Dglgmut <florez.renan@gmail.com>
2013-08-13 22:06:05 -0300

    fixing get changes

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -1,16 +1,16 @@
 class Coins(coinType: Symbol) {
   def value: Double = Coins.coinTypesDictionary(coinType)
-  def toChange(moneyGiven: Double, price: Double): Int = ((price - moneyGiven) / this.value).toInt
-  def changeable(moneyGiven: Double, price: Double): Boolean = toChange(moneyGiven, price) > 0
+  def toChange(price: Double, moneyGiven: Double): Int = ((price - moneyGiven) / this.value).toInt
+  def changeable(price: Double, moneyGiven: Double): Boolean = toChange(moneyGiven, price) > 0
 }
 object Coins{
   private val coinTypesDictionary = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
   def coinTypes: List[Symbol] = coinTypesDictionary.keys.toList
-  def getChanges(moneyGiven: Double, price: Double): List[Any] = {
-    val avaiableCoins = coinTypes.reverse.filter{e => (new Coins(e)).changeable(moneyGiven,price)}
-    if (price - coinTypesDictionary(avaiableCoins.head) == moneyGiven)
-      avaiableCoins.head :: Nil
+  def getChanges(price: Double, moneyGiven: Double): List[Any] = {
+    val avaiableCoins = coinTypes.reverse.filter{e => (new Coins(e)).changeable(price, moneyGiven)}
+    if (avaiableCoins.length > 0 )
+      avaiableCoins.head :: Coins.getChanges(price, moneyGiven - coinTypesDictionary(avaiableCoins.head))
     else
-      avaiableCoins.head :: Coins.getChanges(moneyGiven, price - coinTypesDictionary(avaiableCoins.head))
+      Nil
   }
 }
