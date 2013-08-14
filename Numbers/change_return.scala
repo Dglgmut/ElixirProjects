cf5750fda749f3f8b0e56babc631c29ab5aee71c
Dglgmut <florez.renan@gmail.com>
2013-08-13 21:17:46 -0300

    defining changeable

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -1,6 +1,7 @@
 class Coins(coinType: Symbol) {
   def value: Double = Coins.coinTypesDictionary(coinType)
   def toChange(moneyGiven: Double, price: Double): Int = ((price - moneyGiven) / this.value).toInt
+  def changeable(moneyGiven: Double, price: Double): Boolean = toChange(moneyGiven, price) > 0
 }
 object Coins{
   private val coinTypesDictionary = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
