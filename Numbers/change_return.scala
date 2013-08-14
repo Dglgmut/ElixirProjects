7960f19b73d0c7351d7ab682c70c0ee3e41df1eb
Dglgmut <florez.renan@gmail.com>
2013-08-13 22:27:03 -0300

    grouping coin type and number of coins

--- Numbers/change_return.scala
+++ Numbers/change_return.scala
@@ -6,7 +6,7 @@ class Coins(coinType: Symbol) {
 object Coins{
   private val coinTypesDictionary = Map[Symbol, Double]('penny -> 0.01, 'nickel ->  0.05, 'dime -> 0.10, 'quarter -> 0.25 )
   def coinTypes: List[Symbol] = coinTypesDictionary.keys.toList
-  def getChanges(price: Double, moneyGiven: Double): List[Any] = {
+  def getChanges(price: Double, moneyGiven: Double): List[Symbol] = {
     val avaiableCoins = coinTypes.filter{e => (new Coins(e)).changeable(price, moneyGiven)}
     if (avaiableCoins.length > 0 )
       avaiableCoins.last :: Coins.getChanges(price, moneyGiven - coinTypesDictionary(avaiableCoins.last))
@@ -14,10 +14,12 @@ object Coins{
       Nil
   }
 }
+def countCoinType(coinType: Symbol, coinList: List[Symbol]): Int = coinList.filter(_ == coinType).length
+def groupCoinTypes(coinList: List[Symbol]) = coinList.distinct.map(o => (o.name -> countCoinType(o, coinList)))
 println("Insert the price")
 print("$")
 val price: Double = readLine().toDouble
 println("Insert how much money was given")
 print("$")
 val moneyGiven: Double = readLine().toDouble
-println(Coins.getChanges(price, moneyGiven))
+println(groupCoinTypes(Coins.getChanges(price, moneyGiven)))
