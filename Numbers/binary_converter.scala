d1387423432ca6aee0ca16d462b514e53132d115
Dglgmut <florez.renan@gmail.com>
2013-08-14 21:42:13 -0300

    adding more IO

--- Numbers/binary_converter.scala
+++ Numbers/binary_converter.scala
@@ -1,2 +1,5 @@
-println("Enter the number that you wish to convert to binary")
-println(readLine().toInt.toBinaryString)
+println("Type 'd' for a decimal number or 'b' for binary number")
+if(readLine()(0) == 'd'){
+  println("Enter a decimal to convert to binary")
+  println(readLine().toInt.toBinaryString)
+}
