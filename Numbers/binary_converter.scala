5c6d6abd026495c25f9a28e7d72f66bc08664eba
Dglgmut <florez.renan@gmail.com>
2013-08-14 21:48:41 -0300

    implementing conversion for binary

--- Numbers/binary_converter.scala
+++ Numbers/binary_converter.scala
@@ -2,4 +2,7 @@ println("Type 'd' for a decimal number or 'b' for binary number")
 if(readLine()(0) == 'd'){
   println("Enter a decimal to convert to binary")
   println(readLine().toInt.toBinaryString)
+} else {
+  println("Enter a binary to convert to decimal")
+  println(Integer.parseInt(readLine(),2))
 }
