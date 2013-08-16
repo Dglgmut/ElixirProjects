5f384198cd4487a679d486f1a64dc515d39aa734
Dglgmut <florez.renan@gmail.com>
2013-08-16 15:37:48 -0300

    Update calculator.scala

--- Numbers/calculator.scala
+++ Numbers/calculator.scala
@@ -2,3 +2,5 @@ import scala.reflect.runtime.universe._
 
 val whiteListOperators = List[String]("+","-","/","%")
 println("Insert an expression for the calculator (e.g. '1 + 1' )")
+
+//newTermName("+").encodedName
