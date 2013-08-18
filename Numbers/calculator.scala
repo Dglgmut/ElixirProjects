446aab654efae62d595497d00fe38ffb1e8a017d
Dglgmut <florez.renan@gmail.com>
2013-08-17 23:11:29 -0300

    adding input

--- Numbers/calculator.scala
+++ Numbers/calculator.scala
@@ -3,6 +3,7 @@ import scala.util.matching.Regex
 
 val whiteListOperators = List[String]("+","-","/","%")
 println("Insert an expression for the calculator (e.g. '1 + 1' )")
+val input = readLine()
 
 //Experimental reflection
 val op = typeOf[Int].declaration(newTermName("/").encodedName).asTerm.alternatives(1)//returns / method
