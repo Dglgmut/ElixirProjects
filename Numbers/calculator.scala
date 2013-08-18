e8e75ca46bf4d64ca19ee9fca09d56c70027a6dd
Dglgmut <florez.renan@gmail.com>
2013-08-17 23:21:28 -0300

    adding first and last numbes

--- Numbers/calculator.scala
+++ Numbers/calculator.scala
@@ -7,6 +7,9 @@ val whiteListOperators = List[String]("+","-","/","%")
 println("Insert an expression for the calculator (e.g. '1 + 1' )")
 val input: String = readLine()
 
+val firstNumbers: Int = numbersMatcher.findAllIn(input).toList(0).toInt
+val lastNumbers: Int = numbersMatcher.findAllIn(input).toList(1).toInt
+
 //Experimental reflection
 val op = typeOf[Int].declaration(newTermName("/").encodedName).asTerm.alternatives(1)//returns / method
 runtimeMirror(getClass.getClassLoader).reflect(10).reflectMethod(op.asMethod)(2)//returns 5
