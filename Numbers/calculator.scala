1d63e6c0d3d733c5e2d3965195e0285221223bd8
Dglgmut <florez.renan@gmail.com>
2013-08-17 21:07:45 -0300

    adding reflection for "/" symbol

--- Numbers/calculator.scala
+++ Numbers/calculator.scala
@@ -2,3 +2,8 @@ import scala.reflect.runtime.universe._
 
 val whiteListOperators = List[String]("+","-","/","%")
 println("Insert an expression for the calculator (e.g. '1 + 1' )")
+
+
+//Experimental reflection
+val op = typeOf[Int].declaration(newTermName("/").encodedName).asTerm.alternatives(1)//returns / method
+runtimeMirror(getClass.getClassLoader).reflect(10).reflectMethod(op.asMethod)(2)//returns 5
