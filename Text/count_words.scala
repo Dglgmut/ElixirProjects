f3a055396f8738ee2aac4ddb6d11cff7d5eb129e
Dglgmut <florez.renan@gmail.com>
2013-08-20 22:09:05 -0300

    printing word counter

--- Text/count_words.scala
+++ Text/count_words.scala
@@ -1,3 +1,4 @@
 println("Enter a phrase to count its words")
 val phrase: String = readLine()
 def wordCounter(phrase: String): Integer =  phrase.filter(_ == ' ').size + 1
+println("This phrase has " + wordCounter(phrase) + " words")
