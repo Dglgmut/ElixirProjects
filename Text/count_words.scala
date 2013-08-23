271df823b84618884c9d2f2b528d4648b03e5ed8
Dglgmut <florez.renan@gmail.com>
2013-08-22 23:35:33 -0300

    adding counter for sample file

--- Text/count_words.scala
+++ Text/count_words.scala
@@ -1,3 +1,7 @@
 import scala.io.Source
 def wordCounter(phrase: String): Integer =  phrase.filter(_ == ' ').size + 1
 val source = Source.fromFile("IO/lorem_ipsum")
+var total: Integer = 0
+for (line <- source.getLines())
+  total += wordCounter(line)
+println(total)
