039cc4effd00589b64ba67eb96fac93a85b9c09b
Dglgmut <florez.renan@gmail.com>
2013-08-22 23:48:48 -0300

    adding realine for file path

--- Text/count_words.scala
+++ Text/count_words.scala
@@ -1,6 +1,11 @@
 import scala.io.Source
+
 def wordCounter(phrase: String): Integer =  phrase.filter(_ == ' ').size + 1
-val source = Source.fromFile("IO/lorem_ipsum")
+
+println("Enter the path for a file")
+val path = readLine()
+
+val source = Source.fromFile(path)
 var total: Integer = 0
 for (line <- source.getLines())
   total += wordCounter(line)
